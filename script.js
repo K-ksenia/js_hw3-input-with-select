fb87dd1f88a77016949f3a780ca4a234f8b4eeec
Kseniya Kolesnikova <hyper1995@gmail.com>
2019-01-27 00:17:58 +0300

    Keyboard navigation and other improvements added

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 import { machine, useContext, useState } from './my-state-machine.js'
 
+const minInputLength = 2;
+const target = 'select';
+
+// Пробегаемся по всем элементам и возвращаем тот, у которого data-target равен объявленному target
+function findNodeWithTarget(nodes) {
+    for (let i in nodes) {
+        if (nodes[i].dataset.target === target) {
+            return nodes[i];
+        }
+    }
+}
+
+const inputLines = document.querySelectorAll('input');
+const outputLists = document.querySelectorAll('ul');
+const inputLine = findNodeWithTarget(inputLines);
+const outputList = findNodeWithTarget(outputLists);
+// Задаем стиль отображаемых li элементов
 const outputLineClass = 'contact-form-select__item';
-const inputLine = document.querySelector('.contact-form-input');
-const outputList = document.querySelector('.contact-form-select');
+
 
 const selectMachine = machine({
     id: 'select',
@@ -11,6 +27,7 @@ const selectMachine = machine({
         inputLine: inputLine,
         outputList: outputList,
         outputData: [],
+        selectedId: 0,
         selected: '',
         outputLineClass: outputLineClass
     },
@@ -22,7 +39,7 @@ const selectMachine = machine({
                     service: (event) => {
                         const [context, setContext] = useContext();
                         if (event.id) {
-                            setContext({outputData: [], selected: event.innerHTML});
+                            setContext({selectedId: event.id});
                             useState()[1]('chosen');
                         }
                     }
@@ -35,11 +52,15 @@ const selectMachine = machine({
                     service: (event) => {
                         const [state, setState] = useState();
                         const [context, setContext] = useContext();
-                        if (event.length >= 2) {
+                        if (event.includes(' ')) {
+                            setContext({outputData: ['Строка не должна содержать пробельные символы']});
+                        } else if (event.length >= minInputLength) {
                             window.fetch(`https://api.hh.ru/suggests/areas?text=${event}`)
                                 .then((response) => {
                                     if (response.status !== 200) {
-                                        throw new Error('Error while fetching');
+                                        let error = new Error(response.status);
+                                        error.response = response;
+                                        throw error;
                                     }
                                     return response.json();
                                 })
@@ -52,6 +73,8 @@ const selectMachine = machine({
                                     setState('showing');
                                 })
                                 .catch((err) => {
+                                    console.log('Error while fetching: ' + err.message);
+                                    console.log(err.response);
                                     setContext({outputData: ['Список временно недоступен, проверьте соединение']});
                                     setState('showing');
                                 });
@@ -69,7 +92,7 @@ const selectMachine = machine({
                         const [state, setState] = useState();
                         const [context, setContext] = useContext();
                         if (event.id) {
-                            setContext({outputData: [], selected: event.innerHTML});
+                            setContext({selectedId: event.id});
                             setState('chosen');
                         } else setState('sleeping');
                     }
@@ -80,10 +103,19 @@ const selectMachine = machine({
             onEntry: ['cleanSelectList','showSelectList'],
         },
         chosen: {
-            onEntry: ['cleanSelectList', 'showSelectedItem']
+            onEntry: ['prepareData', 'cleanSelectList', 'showSelectedItem']
         },
     },
     actions: {
+        prepareData : () => {
+            const [context, setContext] = useContext();
+            // ищем индекс элемента с полученным id в массиве outputData
+            const chosenIndex = context.outputData.findIndex((elem, i, arr) => arr[i].id === context.selectedId);
+            setContext({
+                selected: context.outputData[chosenIndex].text,
+                outputData: []
+            });
+        },
         cleanSelectList: () => {
             const [context, setContext] = useContext();
             while (context.outputList.firstElementChild) {
@@ -100,6 +132,7 @@ const selectMachine = machine({
                     } else {
                         li.appendChild(document.createTextNode(context.outputData[i].text));
                         li.setAttribute('id', context.outputData[i].id);
+                        li.setAttribute('tabindex', 0);
                     }
                     li.className = context.outputLineClass;
                     outputList.appendChild(li);
@@ -129,4 +162,58 @@ inputLine.addEventListener('input', () => {
 
 outputList.addEventListener('click', (e) => {
     selectMachine.transition('CHOOSE', e.target)
+});
+
+
+inputLine.addEventListener('keydown', (e) => {
+    if (outputList.childNodes.length !== 0) {
+        // Если есть выпавший список, то передвигаемся по нему
+        switch (e.key) {
+            case 'ArrowDown':
+                outputList.firstElementChild.focus();
+                break;
+            case 'ArrowUp':
+                outputList.lastElementChild.focus();
+                break;
+        }
+    } else {
+        // Иначе убираем фокус с инпута
+        switch (e.key) {
+        case 'ArrowDown':
+        case 'ArrowUp':
+        case 'Tab':
+            inputLine.blur();
+            break;
+    }
+    }
+});
+
+outputList.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case 'ArrowDown':
+            if (e.target !== outputList.lastElementChild) {
+                e.target.nextElementSibling.focus();
+            }
+            break;
+        case 'ArrowUp':
+            if (e.target !== outputList.firstElementChild) {
+                e.target.previousElementSibling.focus();
+            }
+            break;
+        case 'Enter':
+            selectMachine.transition('CHOOSE', e.target);
+            break;
+    }
+});
+
+addEventListener('keydown', (e) => {
+    if (e.target !== inputLine && e.target.className !== outputLineClass) {
+        switch (e.key) {
+            case 'ArrowDown':
+            case 'ArrowUp':
+            case 'Tab':
+                inputLine.focus();
+                break;
+        }
+    }
 });
